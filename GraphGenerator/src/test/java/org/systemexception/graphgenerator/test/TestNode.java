28126e447903979c89c6fa2b7b7b97e14deb8161
Leonardo Cappuccio <leo@systemexception.org>
2015-04-20 00:20:13 +0200

    green: throwExceptionOnNullId

--- GraphGenerator/src/test/java/org/systemexception/graphgenerator/test/TestNode.java
+++ GraphGenerator/src/test/java/org/systemexception/graphgenerator/test/TestNode.java
@@ -7,11 +7,18 @@
 package org.systemexception.graphgenerator.test;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.systemexception.graphgenerator.exception.NodeException;
+import org.systemexception.graphgenerator.model.Node;
 
 public class TestNode {
 
-    public TestNode() {
-    }
+	private Node sut;
+
+	@Test(expected = org.systemexception.graphgenerator.exception.NodeException.class)
+	public void throwExceptionOnNullId() throws NodeException {
+		String nodeId = null;
+		String nodeDescr = "123";
+		sut = new Node(nodeId, nodeDescr);
+	}
 
 }
