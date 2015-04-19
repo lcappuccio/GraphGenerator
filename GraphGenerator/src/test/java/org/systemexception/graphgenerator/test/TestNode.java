b39c8f270383176060499e8d5c710c204352fca4
Leonardo Cappuccio <leo@systemexception.org>
2015-04-20 00:08:57 +0200

    wip

---
+++ GraphGenerator/src/test/java/org/systemexception/graphgenerator/test/TestNode.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author leo
+ * @date 20/04/2015 00:01
+ *
+ */
+package org.systemexception.graphgenerator.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestNode {
+
+    public TestNode() {
+    }
+
+}
