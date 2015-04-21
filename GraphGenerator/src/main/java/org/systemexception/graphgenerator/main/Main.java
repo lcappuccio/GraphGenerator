7a9e9c03d71d729008e6b50bbe098597a6f059f2
Leonardo Cappuccio <leonardo.cappuccio@scytl.com>
2015-04-21 15:21:28 +0200

    sample main

--- GraphGenerator/src/main/java/org/systemexception/graphgenerator/main/Main.java
+++ GraphGenerator/src/main/java/org/systemexception/graphgenerator/main/Main.java
@@ -6,12 +6,30 @@
  */
 package org.systemexception.graphgenerator.main;
 
+import org.systemexception.graphgenerator.exception.CsvWriterException;
+import org.systemexception.graphgenerator.exception.EdgeException;
+import org.systemexception.graphgenerator.exception.NodeException;
+import org.systemexception.graphgenerator.exception.TreeException;
+import org.systemexception.graphgenerator.model.Tree;
+import org.systemexception.graphgenerator.pojo.CsvWriter;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		System.exit(0);
-
+		Tree tree = null;
+		
+		try {
+			tree = new Tree(1,10);
+		} catch (NodeException | EdgeException | TreeException ex) {
+			System.out.println(ex.getMessage());
+		}
+		
+		CsvWriter csvWriter = new CsvWriter("target/output.csv");
+		try {
+			csvWriter.writeFile(tree);
+		} catch (CsvWriterException ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
-
 }
