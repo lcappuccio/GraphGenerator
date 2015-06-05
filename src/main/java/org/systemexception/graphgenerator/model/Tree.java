8849f84fc9d4c5b7bb7a8315e5b375c64505df22
Leonardo Cappuccio <leo@systemexception.org>
2015-06-05 22:27:59 +0200

    green: getParentNode

--- src/main/java/org/systemexception/graphgenerator/model/Tree.java
+++ src/main/java/org/systemexception/graphgenerator/model/Tree.java
@@ -14,168 +14,183 @@ import java.util.List;
 
 public class Tree {
 
-    private final ArrayList<Node> treeNodes;
-    private final ArrayList<Edge> treeEdges;
-    private final ArrayList<ArrayList<String>> treeLevelsString;
-    private final int treeLevels, childPerNode;
+	private final ArrayList<Node> treeNodes;
+	private final ArrayList<Edge> treeEdges;
+	private final ArrayList<ArrayList<String>> treeLevelsString;
+	private final int treeLevels, childPerNode;
 
-    /**
-     * @param levels       the total levels of the tree
-     * @param childPerNode the amount of childs per node
-     * @throws NodeException
-     * @throws EdgeException
-     * @throws TreeException
-     */
-    public Tree(int levels, int childPerNode) throws NodeException, EdgeException, TreeException {
-        treeNodes = new ArrayList();
-        treeEdges = new ArrayList();
-        treeLevelsString = new ArrayList();
-        this.treeLevels = levels;
-        this.childPerNode = childPerNode;
-        Node rootNode = new Node("1", "RootNode");
-        treeNodes.add(rootNode);
-        addTreeLevelForCsvOutput("1", "RootNode", "0", "RootLevel");
-        makeTree(rootNode, 0);
-    }
+	/**
+	 * @param levels       the total levels of the tree
+	 * @param childPerNode the amount of childs per node
+	 * @throws NodeException
+	 * @throws EdgeException
+	 * @throws TreeException
+	 */
+	public Tree(int levels, int childPerNode) throws NodeException, EdgeException, TreeException {
+		treeNodes = new ArrayList();
+		treeEdges = new ArrayList();
+		treeLevelsString = new ArrayList();
+		this.treeLevels = levels;
+		this.childPerNode = childPerNode;
+		Node rootNode = new Node("1", "RootNode");
+		treeNodes.add(rootNode);
+		addTreeLevelForCsvOutput("1", "RootNode", "0", "RootLevel");
+		makeTree(rootNode, 0);
+	}
 
-    /**
-     * Generate the tree recursively
-     *
-     * @param parentNode   the parent node, initially a root node, recursively the parent node
-     * @param currentLevel the current level of the tree
-     * @throws NodeException
-     * @throws EdgeException
-     * @throws TreeException
-     */
-    private void makeTree(Node parentNode, int currentLevel) throws NodeException, EdgeException, TreeException {
-        if (currentLevel == treeLevels) {
-            return;
-        }
-        for (int i = 0; i < childPerNode; i++) {
-            String childNodeId = Labels.NODE_NAME.toString() + parentNode.getNodeId().replace(Labels.NODE_NAME
-                    .toString(), "") + String.valueOf(i);
-            String childNodeDescr = childNodeId + Labels.LEVEL_NAME.toString() + String.valueOf(currentLevel);
-            Node childNode = new Node(childNodeId, childNodeDescr);
-            Edge edge = new Edge(parentNode, childNode);
-            if (nodeExists(childNode.getNodeId())) {
-                throw new TreeException("NodeId " + childNode.getNodeId() + " already exists");
-            }
-            treeNodes.add(childNode);
-            treeEdges.add(edge);
-            addTreeLevelForCsvOutput(childNodeId, childNodeDescr, parentNode.getNodeId(), Labels.LEVEL_NAME.toString
-                    ().replace("_", "") + String.valueOf(currentLevel));
-            makeTree(childNode, currentLevel + 1);
-        }
-    }
+	/**
+	 * Generate the tree recursively
+	 *
+	 * @param parentNode   the parent node, initially a root node, recursively the parent node
+	 * @param currentLevel the current level of the tree
+	 * @throws NodeException
+	 * @throws EdgeException
+	 * @throws TreeException
+	 */
+	private void makeTree(Node parentNode, int currentLevel) throws NodeException, EdgeException, TreeException {
+		if (currentLevel == treeLevels) {
+			return;
+		}
+		for (int i = 0; i < childPerNode; i++) {
+			String childNodeId = Labels.NODE_NAME.toString() + parentNode.getNodeId().replace(Labels.NODE_NAME
+					.toString(), "") + String.valueOf(i);
+			String childNodeDescr = childNodeId + Labels.LEVEL_NAME.toString() + String.valueOf(currentLevel);
+			Node childNode = new Node(childNodeId, childNodeDescr);
+			Edge edge = new Edge(parentNode, childNode);
+			if (nodeExists(childNode.getNodeId())) {
+				throw new TreeException("NodeId " + childNode.getNodeId() + " already exists");
+			}
+			treeNodes.add(childNode);
+			treeEdges.add(edge);
+			addTreeLevelForCsvOutput(childNodeId, childNodeDescr, parentNode.getNodeId(), Labels.LEVEL_NAME.toString
+					().replace("_", "") + String.valueOf(currentLevel));
+			makeTree(childNode, currentLevel + 1);
+		}
+	}
 
-    /**
-     * Creates a String representation of a tree level
-     *
-     * @param childNodeId
-     * @param childNodeDescr
-     * @param nodeId
-     * @param e
-     */
-    private void addTreeLevelForCsvOutput(String childNodeId, String childNodeDescr, String nodeId, String e) {
-        ArrayList<String> treeLevelString = new ArrayList();
-        treeLevelString.add(childNodeId);
-        treeLevelString.add(nodeId);
-        treeLevelString.add(childNodeDescr);
-        treeLevelString.add(e);
-        treeLevelsString.add(treeLevelString);
-    }
+	/**
+	 * Creates a String representation of a tree level
+	 *
+	 * @param childNodeId
+	 * @param childNodeDescr
+	 * @param nodeId
+	 * @param e
+	 */
+	private void addTreeLevelForCsvOutput(String childNodeId, String childNodeDescr, String nodeId, String e) {
+		ArrayList<String> treeLevelString = new ArrayList();
+		treeLevelString.add(childNodeId);
+		treeLevelString.add(nodeId);
+		treeLevelString.add(childNodeDescr);
+		treeLevelString.add(e);
+		treeLevelsString.add(treeLevelString);
+	}
 
-    /**
-     * Empties a tree
-     */
-    public void emptyTree() {
-        Node startingNode = treeNodes.get(treeNodes.size() - 1);
-        while (treeNodes.size() > 0) {
-            removeNode(startingNode);
-            if (treeNodes.size() > 0) {
-                emptyTree();
-            }
-        }
-    }
+	/**
+	 * Empties a tree
+	 */
+	public void emptyTree() {
+		Node startingNode = treeNodes.get(treeNodes.size() - 1);
+		while (treeNodes.size() > 0) {
+			removeNode(startingNode);
+			if (treeNodes.size() > 0) {
+				emptyTree();
+			}
+		}
+	}
 
-    /**
-     * Removes a node from the tree
-     *
-     * @param node the node to remove
-     * @throws TreeException
-     */
-    public void removeNode(Node node) {
-        ArrayList<Node> childNodes = getChildNodes(node);
-        if (getChildNodes(node).size() > 0) {
-            for (Node childNode : childNodes) {
-                System.out.println("Found child node: " + childNode.getNodeId() + " for node " + node.getNodeId());
-            }
-        } else {
-            if (treeNodes.contains(node)) {
-                System.out.println("Remove node: " + node.getNodeId());
-                treeNodes.remove(node);
-            }
-            removeIncomingEdgeTo(node);
-        }
-    }
+	/**
+	 * Removes a node from the tree
+	 *
+	 * @param node the node to remove
+	 * @throws TreeException
+	 */
+	public void removeNode(Node node) {
+		ArrayList<Node> childNodes = getChildNodes(node);
+		if (getChildNodes(node).size() > 0) {
+			for (Node childNode : childNodes) {
+				System.out.println("Found child node: " + childNode.getNodeId() + " for node " + node.getNodeId());
+			}
+		} else {
+			if (treeNodes.contains(node)) {
+				System.out.println("Remove node: " + node.getNodeId());
+				treeNodes.remove(node);
+			}
+			removeIncomingEdgeTo(node);
+		}
+	}
 
-    /**
-     * Removes an incoming edge to this node
-     *
-     * @param node
-     */
-    private void removeIncomingEdgeTo(Node node) {
-        List<Edge> edgesToRemove = new ArrayList<>();
-        for (Edge edge : treeEdges) {
-            if (edge.getChildNode().equals(node)) {
-                edgesToRemove.add(edge);
-            }
-        }
-        for (Edge edgeToRemove : edgesToRemove) {
-            treeEdges.remove(edgeToRemove);
-        }
-    }
+	/**
+	 * Removes an incoming edge to this node
+	 *
+	 * @param node
+	 */
+	private void removeIncomingEdgeTo(Node node) {
+		List<Edge> edgesToRemove = new ArrayList<>();
+		for (Edge edge : treeEdges) {
+			if (edge.getChildNode().equals(node)) {
+				edgesToRemove.add(edge);
+			}
+		}
+		for (Edge edgeToRemove : edgesToRemove) {
+			treeEdges.remove(edgeToRemove);
+		}
+	}
 
-    /**
-     * Returns the child nodes of a given node
-     *
-     * @param node the node to check
-     * @return
-     */
-    public ArrayList<Node> getChildNodes(Node node) {
-        ArrayList<Node> childNodes = new ArrayList<>();
-        for (Edge edge : treeEdges) {
-            if (edge.getParentNode().equals(node)) {
-                childNodes.add(edge.getChildNode());
-            }
-        }
-        return childNodes;
-    }
+	/**
+	 * Returns the child nodes of a given node
+	 *
+	 * @param node the node to check
+	 * @return
+	 */
+	public ArrayList<Node> getChildNodes(Node node) {
+		ArrayList<Node> childNodes = new ArrayList<>();
+		for (Edge edge : treeEdges) {
+			if (edge.getParentNode().equals(node)) {
+				childNodes.add(edge.getChildNode());
+			}
+		}
+		return childNodes;
+	}
 
-    /**
-     * Verify if nodeId already exists in tree
-     *
-     * @param nodeId the node id to check
-     * @return the boolean verification value
-     */
-    private boolean nodeExists(String nodeId) {
-        for (Node treeNode : treeNodes) {
-            if (treeNode.getNodeId().equals(nodeId)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	/**
+	 * Returns the parent node of a given node
+	 *
+	 * @param node the node to check
+	 * @return
+	 */
+	public Node getParentNode(Node node) {
+		for (Edge edge : treeEdges) {
+			if (edge.getChildNode().equals(node)) {
+				return edge.getParentNode();
+			}
+		}
+		return null;
+	}
 
-    public List<Node> getNodes() {
-        return treeNodes;
-    }
+	/**
+	 * Verify if nodeId already exists in tree
+	 *
+	 * @param nodeId the node id to check
+	 * @return the boolean verification value
+	 */
+	private boolean nodeExists(String nodeId) {
+		for (Node treeNode : treeNodes) {
+			if (treeNode.getNodeId().equals(nodeId)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public List<Edge> getEdges() {
-        return treeEdges;
-    }
+	public List<Node> getNodes() {
+		return treeNodes;
+	}
 
-    public ArrayList<ArrayList<String>> getTreeLevelsString() {
-        return treeLevelsString;
-    }
+	public List<Edge> getEdges() {
+		return treeEdges;
+	}
+
+	public ArrayList<ArrayList<String>> getTreeLevelsString() {
+		return treeLevelsString;
+	}
 }
--- src/test/java/org/systemexception/graphgenerator/test/TestTree.java
+++ src/test/java/org/systemexception/graphgenerator/test/TestTree.java
@@ -13,84 +13,95 @@ import org.systemexception.graphgenerator.model.Tree;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class TestTree {
 
-    private Tree sut;
-    double totalNodes;
+	private Tree sut;
+	double totalNodes;
 
-    @Test
-    public void createSimpleTree() throws NodeException, EdgeException, TreeException {
-        generateTree(1, 3);
-        assertTrue(totalNodes == sut.getNodes().size());
-    }
+	@Test
+	public void createSimpleTree() throws NodeException, EdgeException, TreeException {
+		generateTree(1, 3);
+		assertTrue(totalNodes == sut.getNodes().size());
+	}
 
-    @Test
-    public void createTwoLevelTree() throws NodeException, EdgeException, TreeException {
-        generateTree(2, 2);
-        assertTrue(totalNodes == sut.getNodes().size());
-    }
+	@Test
+	public void createTwoLevelTree() throws NodeException, EdgeException, TreeException {
+		generateTree(2, 2);
+		assertTrue(totalNodes == sut.getNodes().size());
+	}
 
-    @Test
-    public void createFourLevelTree() throws NodeException, EdgeException, TreeException {
-        generateTree(2, 4);
-        assertTrue(totalNodes == sut.getNodes().size());
-    }
+	@Test
+	public void createFourLevelTree() throws NodeException, EdgeException, TreeException {
+		generateTree(2, 4);
+		assertTrue(totalNodes == sut.getNodes().size());
+	}
 
-    @Test
-    public void createModerateTree() throws NodeException, EdgeException, TreeException {
-        generateTree(5, 6);
-        assertTrue(totalNodes == sut.getNodes().size());
-    }
+	@Test
+	public void createModerateTree() throws NodeException, EdgeException, TreeException {
+		generateTree(5, 6);
+		assertTrue(totalNodes == sut.getNodes().size());
+	}
 
-    @Test
-    public void checkIfRootHasChilds() throws NodeException, EdgeException, TreeException {
-        generateTree(1, 3);
-        ArrayList<Node> childNodes = sut.getChildNodes(sut.getNodes().get(0));
-        assertTrue(!childNodes.isEmpty());
-    }
+	@Test
+	public void checkIfRootHasChilds() throws NodeException, EdgeException, TreeException {
+		generateTree(1, 3);
+		ArrayList<Node> childNodes = sut.getChildNodes(sut.getNodes().get(0));
+		assertTrue(!childNodes.isEmpty());
+	}
 
-    @Test
-    public void dontRemoveNodeWithChilds() throws NodeException, TreeException, EdgeException {
-        generateTree(1, 3);
-        Node rootNode = sut.getNodes().get(0);
-        sut.removeNode(rootNode);
-        assertTrue(sut.getNodes().contains(rootNode));
-    }
+	@Test
+	public void dontRemoveNodeWithChilds() throws NodeException, TreeException, EdgeException {
+		generateTree(1, 3);
+		Node rootNode = sut.getNodes().get(0);
+		sut.removeNode(rootNode);
+		assertTrue(sut.getNodes().contains(rootNode));
+	}
 
-    @Test
-    public void emptyTree() throws NodeException, TreeException, EdgeException {
-        generateTree(3, 3);
-        while (sut.getNodes().size() > 0) {
-            for (int i = 0; i < sut.getNodes().size(); i++) {
-                Node nodeToRemove = sut.getNodes().get(i);
-                sut.removeNode(nodeToRemove);
-            }
-        }
-        assertTrue(sut.getNodes().size() == 0);
-        assertTrue(sut.getEdges().size() == 0);
-    }
+	@Test
+	public void emptyTree() throws NodeException, TreeException, EdgeException {
+		generateTree(3, 3);
+		while (sut.getNodes().size() > 0) {
+			for (int i = 0; i < sut.getNodes().size(); i++) {
+				Node nodeToRemove = sut.getNodes().get(i);
+				sut.removeNode(nodeToRemove);
+			}
+		}
+		assertTrue(sut.getNodes().size() == 0);
+		assertTrue(sut.getEdges().size() == 0);
+	}
 
-   @Test
-   public void emptyTreeInternalMethod() throws NodeException, TreeException, EdgeException {
-       generateTree(4,6);
-       sut.emptyTree();
-       assertTrue(sut.getNodes().size() == 0);
-       assertTrue(sut.getEdges().size() == 0);
-   }
+	@Test
+	public void emptyTreeInternalMethod() throws NodeException, TreeException, EdgeException {
+		generateTree(4, 6);
+		sut.emptyTree();
+		assertTrue(sut.getNodes().size() == 0);
+		assertTrue(sut.getEdges().size() == 0);
+	}
 
-    /**
-     * Generates a tree
-     *
-     * @param height       the height of the tree level we're on
-     * @param childPerNode how many childs per node
-     * @throws NodeException
-     * @throws EdgeException
-     * @throws TreeException
-     */
-    private void generateTree(int height, int childPerNode) throws NodeException, EdgeException, TreeException {
-        totalNodes = (Math.pow(childPerNode, height + 1) - 1) / (childPerNode - 1);
-        sut = new Tree(height, childPerNode);
-    }
+	@Test
+	public void getParentNode() throws NodeException, TreeException, EdgeException {
+		generateTree(1,1);
+		Node childNode = sut.getNodes().get(sut.getNodes().size() - 1);
+		Node parentNode = sut.getNodes().get(0);
+		assertEquals(sut.getParentNode(childNode), parentNode);
+		assertEquals(sut.getParentNode(parentNode), null);
+	}
+
+
+	/**
+	 * Generates a tree
+	 *
+	 * @param height       the height of the tree level we're on
+	 * @param childPerNode how many childs per node
+	 * @throws NodeException
+	 * @throws EdgeException
+	 * @throws TreeException
+	 */
+	private void generateTree(int height, int childPerNode) throws NodeException, EdgeException, TreeException {
+		totalNodes = (Math.pow(childPerNode, height + 1) - 1) / (childPerNode - 1);
+		sut = new Tree(height, childPerNode);
+	}
 }
